6a5437502632432b25f97914e81b77e9ef6a87bf
Vuyo <joymama078@gmail.com>
2023-02-24 16:16:41 +0200

    Add request mapping to user controller

--- api/src/main/java/org/example/controllers/UserController.java
+++ api/src/main/java/org/example/controllers/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1")
 public class UserController {
     private final UserRepository userRepository;
 
