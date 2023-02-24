e58a02c3dd6b03e0ff51aeb8cc3a72047743c753
Vuyo <joymama078@gmail.com>
2023-02-24 15:54:43 +0200

    Create User repository

---
+++ api/src/main/java/org/example/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.repositories;
+
+import org.example.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, UUID> {
+}
