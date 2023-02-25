3ec8d60fd631a975cb2a88ce546314e4c1e636a4
Vuyo <joymama078@gmail.com>
2023-02-25 08:18:29 +0200

    Create user creation dto

---
+++ api/src/main/java/org/example/users/dto/UserCreationDTO.java
@@ -0,0 +1,22 @@
+package org.example.users.dto;
+
+public class UserCreationDTO {
+    private String emailAddress;
+    private String password;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
