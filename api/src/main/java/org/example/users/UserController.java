6b322d00c0ad6c40da893c26fd480d602350e7f2
Vuyo <joymama078@gmail.com>
2023-02-25 15:51:13 +0200

    Code formatting and clean up

--- api/src/main/java/org/example/users/UserController.java
+++ api/src/main/java/org/example/users/UserController.java
@@ -1,12 +1,14 @@
 package org.example.users;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.example.users.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 import static org.example.config.SecurityConfiguration.SECURITY_CONFIG_NAME;
 
@@ -22,7 +24,14 @@ public class UserController {
 
     @GetMapping(value = "all/")
     public ResponseEntity<Object> getAllUsers() {
-        Iterable<User> users = userRepository.findAll();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(users);
+        List<UserResponse> users = StreamSupport.stream(userRepository.findAll().spliterator(), false)
+                .map(user -> {
+                    UserResponse ur = new UserResponse();
+                    ur.setEmailAddress(user.getEmail());
+                    ur.setFirstName(user.getFirstName());
+                    ur.setLastName(user.getLastName());
+                    return ur;
+                }).toList();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 }
