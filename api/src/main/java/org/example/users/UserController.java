3052e5bbddad49ea453ccd035588f721bbc60755
Vuyo <joymama078@gmail.com>
2023-02-25 08:29:24 +0200

    Add request mapping to UserController class

--- api/src/main/java/org/example/users/UserController.java
+++ api/src/main/java/org/example/users/UserController.java
@@ -3,12 +3,10 @@ package org.example.users;
 import org.example.users.dto.UserCreationDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/users/")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -16,13 +14,13 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping(value = "/users/all/")
+    @GetMapping(value = "/all/")
     public ResponseEntity<Object> getAllUsers() {
         Iterable<UserModel> users = userRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
-    @PostMapping(value = "/users/create/")
+    @PostMapping(value = "/auth/register/")
     public ResponseEntity<Object> createNewUser(@RequestBody UserCreationDTO userModel) {
         System.out.println(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body("yes");
