a02ac310b95c01f20d9188c0dad106b4675fa557
Vuyo <joymama078@gmail.com>
2023-02-25 15:33:35 +0200

    Add authorisation mechanism in Swagger UI

--- api/src/main/java/org/example/config/SecurityConfiguration.java
+++ api/src/main/java/org/example/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package org.example.config;
 
-import lombok.RequiredArgsConstructor;
+import static io.swagger.v3.oas.annotations.enums.SecuritySchemeIn.HEADER;
+import static io.swagger.v3.oas.annotations.enums.SecuritySchemeType.HTTP;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -12,18 +14,23 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@SecurityScheme(name = SecurityConfiguration.SECURITY_CONFIG_NAME, in = HEADER, type = HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
+    public static final String SECURITY_CONFIG_NAME = "App Bearer token";
     private static final String[] AUTH_WHITELIST = {
             "/api/v1/users/auth/**",
             "/swagger-ui/**",
             "/api-docs/**"
     };
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
---
+++ api/src/main/java/org/example/users/UserController.java
@@ -0,0 +1,29 @@
+package org.example.users;
+
+import static org.example.config.SecurityConfiguration.SECURITY_CONFIG_NAME;
+
+import org.example.users.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@RestController
+@SecurityRequirement(name = SECURITY_CONFIG_NAME)
+@RequestMapping(value = "/api/v1/users/")
+public class UserController {
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping(value = "all/")
+    public ResponseEntity<Object> getAllUsers() {
+        Iterable<User> users = userRepository.findAll();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(users);
+    }
+}
