0781464342bf9d68c75403db241a6b618a13156d
McQuile Karappen <mcquile@bbd.co.za>
2023-02-28 19:04:24 +0200

    Updated user-created-polls script to use the poll service

--- Frontend/poll-position-angular/src/app/user-created-polls/user-created-polls.component.ts
+++ Frontend/poll-position-angular/src/app/user-created-polls/user-created-polls.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Poll} from "../models/poll";
+import {PollService} from "../poll.service";
 
 @Component({
   selector: 'app-user-created-polls',
   templateUrl: './user-created-polls.component.html',
   styleUrls: ['./user-created-polls.component.css']
 })
-export class UserCreatedPollsComponent {
+export class UserCreatedPollsComponent implements OnInit{
   polls: Poll[] = [];
+
+  constructor(private pollService: PollService) {}
+
+  getUserCreatedPolls(): void {
+    this.pollService.getUserCreatedPolls().subscribe(polls => this.polls = polls);
+  }
+
+  ngOnInit(): void {
+    this.getUserCreatedPolls();
+  }
 }
