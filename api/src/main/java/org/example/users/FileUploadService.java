61e7f1e8ad8010cbf65de2f0d3461d9fefaa278a
Vuyo <joymama078@gmail.com>
2023-02-28 13:07:39 +0200

    Create profile upload endpoint

---
+++ api/src/main/java/org/example/users/FileUploadService.java
@@ -0,0 +1,55 @@
+package org.example.users;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+@Service
+public class FileUploadService {
+    private static final String DOWNLOAD_URL = "https://storage.googleapis.com/profile_pics_bucket/%s";
+
+    public String uploadFile(File file, String fileName) throws IOException, URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("credentials.json");
+        File credentialsFile;
+
+        if (resource == null) {
+            throw new IllegalArgumentException("Firebase credentials not found!");
+        } else {
+            credentialsFile = new File(resource.toURI());
+        }
+
+        BlobId blobId = BlobId.of("profile_pics_bucket", fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsFile));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+        return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+    }
+
+    public File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File tempFile = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.write(multipartFile.getBytes());
+        }
+        return tempFile;
+    }
+
+    public String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+}
--- api/src/main/java/org/example/users/UserController.java
+++ api/src/main/java/org/example/users/UserController.java
@@ -1,31 +1,37 @@
 package org.example.users;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import lombok.RequiredArgsConstructor;
 import org.example.branches.BranchRepository;
 import org.example.entities.Branch;
 import org.example.users.dto.UserDTO;
 import org.example.users.models.User;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import static org.example.config.SecurityConfiguration.SECURITY_CONFIG_NAME;
 
 @RestController
 @SecurityRequirement(name = SECURITY_CONFIG_NAME)
 @RequestMapping(value = "/api/v1/users")
-@RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
     private final BranchRepository branchRepository;
+
+    public UserController(UserRepository userRepository, BranchRepository branchRepository) {
+        this.userRepository = userRepository;
+        this.branchRepository = branchRepository;
+    }
 
     @GetMapping
     public ResponseEntity<Object> getAllUsers() {
@@ -60,5 +66,19 @@ public class UserController {
         User response = userRepository.save(user);
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+    }
+
+    @PostMapping(value = "/profile-pics/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws IOException, URISyntaxException {
+        FileUploadService fileUploadService = new FileUploadService();
+
+        String fileName = file.getOriginalFilename();
+        fileName = UUID.randomUUID().toString().concat(fileUploadService.getExtension(fileName));
+
+        File f = fileUploadService.convertToFile(file, file.getOriginalFilename());
+        String response = fileUploadService.uploadFile(f, fileName);
+        f.delete();
+
+        return ResponseEntity.ok(response);
     }
 }
