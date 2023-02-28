3bf534b8c1c853f3835ac9fa8f76d6e93d95161e
McQuile Karappen <mcquile@bbd.co.za>
2023-02-28 20:26:58 +0200

    Generated create-poll component

--- Frontend/poll-position-angular/src/app/app.module.ts
+++ Frontend/poll-position-angular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserCreatedPollsComponent } from './user-created-polls/user-created-polls.component';
 import { AvailablePollsComponent } from './available-polls/available-polls.component';
+import { CreatePollComponent } from './create-poll/create-poll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     UserCreatedPollsComponent,
-    AvailablePollsComponent
+    AvailablePollsComponent,
+    CreatePollComponent
   ],
   imports: [
     BrowserModule
